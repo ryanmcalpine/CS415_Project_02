d8fb656c4a876913df9e3128564bd0950df14060
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-04-01 17:22:05 -0700

    Continued work on karatsuba_mult()
    Function now works properly for all cases except multiplication of an input 3-digit number with any number less than 1000, which still produces an incorrect result.
    
    Added some comments to make code more legible.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import math
 
-
 def karatsuba_mult( A, B ):
+    # Base case: single digits
     if len(A) == 1 and len(B) == 1:
         return A[0] * B[0]
 
+    # Ensure arrays' int values are both <= 1000
     num1 = ""
     for c in A:
         num1 += str(c)
@@ -14,19 +15,21 @@ def karatsuba_mult( A, B ):
     if int(num1) > 1000 or int(num2) > 1000:
         return -1
 
+    # Ensure arrays are same length by padding 0's if necessary
     while len(A) < len(B):
         A.insert(0, 0)
     while len(B) < len(A):
         B.insert(0, 0)
 
-    m1 = math.floor(len(A)//2)
-    m2 = math.floor(len(B)//2)
+    # m = middle index
+    m = math.floor(len(A)//2)
 
-    x1 = A[:m1]
-    x0 = A[m1:] if len(A) % 2 == 0 else A[m1+1:]
+    # split arrays, account for odd array lengths in x0
+    x1 = A[:m]
+    x0 = A[m:] if len(A) % 2 == 0 else A[m+1:]
 
-    y1 = B[:m2]
-    y0 = B[m2:] if len(B) % 2 == 0 else B[m2 + 1:]
+    y1 = B[:m]
+    y0 = B[m:] if len(B) % 2 == 0 else B[m+1:]
 
     # z0 = x0y0
     # z1 = x1y0 + x0y1 = (x1 + x0)(y1 + y0) - x1y1 - x0y0
@@ -36,36 +39,48 @@ def karatsuba_mult( A, B ):
     z2 = karatsuba_mult(x1, y1)
     z1 = karatsuba_mult(add_arrays(x1, x0), add_arrays(y1, y0)) - z2 - z0
 
-    return (z2 * math.pow(10, m2 * 2)) + ((z1 - z2 - z0) * math.pow(10, m2)) + z0
+    # xy = z2(B^2m) + z1(B^m) + z0
+    # base 10 --> B = 10
+    return (z2 * math.pow(10, m * 2)) + (z1 * math.pow(10, m)) + z0
 
 def add_arrays(A, B):
+    # Ensure the arrays are of even length by padding 0's if necessary
     while len(A) < len(B):
         A.insert(0, 0)
     while len(B) < len(A):
         B.insert(0, 0)
 
-    C = []
-    i = 0
+    C = []  # return value is also an array
+    i = len(A) - 1  # start with least-significant digit
     carry = 0
-    for n in A:
+    while i >= 0:   # for each digit in arrays
         c = A[i] + B[i]
         c += carry
-        if c > 9:
+        if c > 9:   # if c is 2 digits, carry the extra
             carry = c / 10
             c = c % 10
         else:
             carry = 0
-        C.insert(0, c)
-        i += 1
+        C.insert(0, int(c))
+        i -= 1
+
+    # Account for possible C of length len(A)+1 (e.g. 5 + 5 = 10)
+    if carry != 0:
+        C.insert(0, int(carry))
 
     return C
 
 def main():
+    # Get input values
     num1 = input("> Enter a number A (<= 1000) to multiply using the Karatsuba Algorithm:\n")
     num2 = input("> Enter a number B (<= 1000) to multiply using the Karatsuba Algorithm:\n")
+
+    # Store values as digit arrays
     A = [int(n) for n in str(num1)]
     B = [int(n) for n in str(num2)]
-    print(karatsuba_mult(A, B), "\n")
+
+    # Output result
+    print("\n = ", int(karatsuba_mult(A, B)), "\n")
 
 if __name__ == "__main__":
     while True:
